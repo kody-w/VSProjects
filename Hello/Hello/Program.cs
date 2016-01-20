0e72b493a67623d6d7985e374fec1138044ad799
wildfeuer <kwildfeuer@me.com>
2016-01-20 06:41:48 -0600

    Added examples from Udemy section 2,3

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your name:");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("How many hours of sleep did you get last night?");
+
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+
+            
+
+            Console.WriteLine("Hello, " + name);
+
+            if(hoursOfSleep >= 8)
+            {
+                Console.WriteLine("You are well rested");
+
+            }
+            else
+            {
+                Console.WriteLine("You need more sleep");
+
+            }
+            
+
+        }
+    }
+}
